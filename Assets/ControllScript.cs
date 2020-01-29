7f71a589617c548b9d1d437662181888dfa20d1a
Timo <gerthner@gmail.com>
2020-01-29 23:10:09 +0100

    add functions for  killing and refilling ghosts checklocations is bullshit

--- Assets/ControllScript.cs
+++ Assets/ControllScript.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ControllScript : MonoBehaviour {
     private List<GhostController> ghosts;
@@ -21,10 +24,26 @@ public class ControllScript : MonoBehaviour {
                 ghost.Move(2);
                 ghost.Rotate(Random.value * 90);
             }
+            
+            GenerateRandomCommand();
 
-            robot.CompareLocations(ghosts[0].GetPosition(), ghosts[0].GetRotation());
-            Debug.Log(robot.Scan());
-            float distance = robot.Scan();
+            // töte alle ghosts out of map
+            KillOutOfMapGhosts();
+            
+            // töte schlechte ghosts
+            KillBadGhosts();
+            
+            // add new ghosts till amount is 200
+            FillUpGhosts();
+
+            // abbruchbedingung
+            if (CheckLocations()) {
+                Debug.Log("final!");
+                return;
+            }
+            // robot.CompareLocations(ghosts[0].GetPosition(), ghosts[0].GetRotation());
+            // Debug.Log(robot.Scan());
+            
             GenerateRandomCommand();
             robotReady = false;
         }
@@ -48,7 +67,7 @@ public class ControllScript : MonoBehaviour {
         }
     }
 
-    public void KillOutOfMapGhosts() {
+    private void KillOutOfMapGhosts() {
         foreach (GhostController ghost in ghosts) {
             float x = ghost.GetPosition().x;
             float z = ghost.GetPosition().y;
@@ -59,6 +78,44 @@ public class ControllScript : MonoBehaviour {
                 CreateGhost(Ghostspawner);
             }
         }
+    }
+
+    private void KillBadGhosts() {
+        List<float> differences = new List<float>();
+        foreach (var ghost in ghosts) {
+            differences.Add(Mathf.Abs(ghost.GetDistance() - robot.Scan()));
+        }
+
+        float mean = differences.Sum() / this.ghosts.Count;
+
+        foreach (var ghost in ghosts) {
+            float differenceToRobotDistance = Mathf.Abs(ghost.GetDistance() - robot.Scan());
+            if (differenceToRobotDistance > mean) {
+                Destroy(ghost);
+            }
+        }
+    }
+
+    private void FillUpGhosts() {
+        int ghostsCount = ghosts.Count;
+        Debug.Log("GhostCount after killing: " + ghostsCount);
+
+        int amountOfFillUpGhosts = 200 - ghostsCount;
+        for (int i = 0; i < amountOfFillUpGhosts; i++) {
+            Ghostspawner.position = new Vector3(Random.Range(-49.0f, 49.0f), 0.5f, Random.Range(-100.0f, 100.0f));
+            CreateGhost(Ghostspawner);
+        }
+    }
+
+    private Boolean CheckLocations() {
+        List<float> locationComparisons = new List<float>();
+        
+
+        if (locationComparisons.Sum() < 30) {
+            return true;
+        }
+
+        return false;
     }
 
     private void CreateGhost(Transform trans) {
